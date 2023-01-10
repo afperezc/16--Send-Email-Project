a61ab23c655d1533a9e7269c5717ba978d352701
Andres Contreras <afperezcontreras@gmail.com>
2023-01-10 11:40:05 -0600

    HTML & CSS, Initializing JS

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function (){ //Cuando se carga el documento
+    
+    //Seleccionar los elementos de la interfaz
+
+    const inputEmail = document.querySelector('#email');
+
+    console.log(inputEmail);
+});
